65dc007e27c83b6995ba1a6a266c41d26a0b8ebd
bani2016up <bani2016up@gmail.com>
2024-12-08 20:18:01 +0300

    update docker

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
 
 
-cd app/frontend
-echo "Stage 1: Installing dependency for frontend."
-poetry lock
-poetry install
-cd ../
-cd backend
-echo "Stage 2: Installing dependency for backend."
-poetry lock
-poetry install
-cd ../../
-echo ls
-echo "Stage 3: Starting docker compose web app."
+echo "Starting docker compose web app."
 docker compose up -d --no-deps --build
